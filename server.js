6070e62bbe91f9bdf9926d1cb84a818cad993488
matthewvolk <volkmattj@gmail.com>
2018-10-21 09:52:11 -0700

    configure rendering engine, static file directory, dev server logging

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
-app.get('/', (req, res) => res.json({ HealthiestThingAt: "The only menu you can trust in the drive thru." }))
+app.use(morgan('dev'));
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => res.render('index'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
