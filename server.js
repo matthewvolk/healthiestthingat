288eafe977d9b11c250f128dd966e8f82a4a5b5b
matthewvolk <volkmattj@gmail.com>
2018-10-21 01:22:40 -0700

    change copy

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => res.json({ HealthiestThingAt: "The only menu you should look at in the drive thru." }))
+app.get('/', (req, res) => res.json({ HealthiestThingAt: "The only menu you can trust in the drive thru." }))
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
