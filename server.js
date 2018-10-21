9006c15c02ccab33994e10884f91b6cca1b8cd7f
matthewvolk <volkmattj@gmail.com>
2018-10-21 12:11:12 -0700

    search success

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const path = require('path');
 const express = require('express');
-const app = express();
 const morgan = require('morgan');
 const favicon = require('serve-favicon')
+
+const app = express();
 
 app.use(morgan('dev'));
 
@@ -13,6 +14,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 
 app.get('/', (req, res) => res.render('index'));
+
+app.post('/search', (req, res) => {
+  res.json({ search: 'success' });
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`HTA listening on port ${PORT}`));
