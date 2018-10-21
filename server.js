70ed2d3937a7cbe8449e6b66ac8ca29d7a6836e1
matthewvolk <volkmattj@gmail.com>
2018-10-21 11:37:05 -0700

    add favicon, change input type

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const favicon = require('serve-favicon')
 
 app.use(morgan('dev'));
 
@@ -9,6 +10,7 @@ app.set('view engine', 'ejs');
 app.set('views', './views');
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 
 app.get('/', (req, res) => res.render('index'));
 
