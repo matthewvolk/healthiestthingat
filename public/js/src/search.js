1881ca1e9b1369c40e2a4f862e49fedc0551f34e
matthewvolk <volkmattj@gmail.com>
2018-10-29 22:33:51 -0700

    add comments to think through logic

--- public/js/src/search.js
+++ public/js/src/search.js
@@ -2,6 +2,7 @@ $(function () {
   $('#searchForm').on('submit', function(event) {
     event.preventDefault();
 
+    // Grab input field HTML element with ID searchQuery
     var searchQuery = $('#searchQuery');
 
     $.ajax({
@@ -9,10 +10,16 @@ $(function () {
       method: 'POST',
       contentType: 'application/json',
       data: JSON.stringify({ restaurantQuery: searchQuery.val() }),
+
       success: function(response) {
+        
+        // Begin rendering data returned from server after form is submitted
         $( ".hero-browser-inner" ).empty().append( JSON.stringify(response).replace(/\"/g, "") );
+
+        // Clear search input field
         searchQuery.val('');
       }
     })
+
   })
 })
--- server.js
+++ server.js
@@ -1,9 +1,15 @@
+/* ----------------- *\
+    Imports
+\* ----------------- */
 const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const favicon = require('serve-favicon')
 const bodyParser = require('body-parser');
 
+/* ----------------- *\
+    Setup & Config
+\* ----------------- */
 const app = express();
 
 app.use(morgan('dev'));
@@ -16,14 +22,20 @@ app.set('views', './views');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 
+/* ----------------- *\
+    Routes
+\* ----------------- */
 app.get('/', (req, res) => res.render('index'));
 
 app.post('/search', (req, res) => {
   var restaurantQuery = req.body.restaurantQuery;
 
-  res.json(`Nutritional data on ${restaurantQuery} is coming soon!`);
+  res.send(`Nutritional data on ${restaurantQuery} is coming soon!`);
 });
 
+/* ----------------- *\
+    Server
+\* ----------------- */
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`app listening on port ${PORT}`));
 
