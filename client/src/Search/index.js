d954da9e5aa90c1b908c8116f03d6596d29ba56f
matthewvolk <volkmattj@gmail.com>
2018-11-21 08:54:08 -0800

    add input change handlers for Search component

--- client/src/Search/index.js
+++ client/src/Search/index.js
@@ -1,11 +1,40 @@
 import React, { Component } from 'react';
+import './index.css';
 
 class Search extends Component {
+  state = {
+    query: '',
+  }
+
+  componentDidMount () {
+    this.search.focus();
+  }
+
+  handleInputChange = () => {
+    this.setState({
+      query: this.search.value
+    })
+  }
+
   render () {
     return (
       <form>
-        <input type="search" name="" id=""/>
-        <input type="submit" value="Search"/>
+        <input 
+          type="search"
+          placeholder="Try typing 'Burger King' or 'McDonald's'"
+          ref={input => this.search = input}
+          onChange={this.handleInputChange}
+          name="" 
+          id=""
+        />
+
+        <input 
+          type="submit" 
+          value="Search" 
+        />
+
+        <p>{this.state.query}</p>
+
       </form>
     )
   }
