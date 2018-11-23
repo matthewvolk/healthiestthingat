be0e08b99ecdf6dfb7771a72f54f033fd75d3f9a
matthewvolk <volkmattj@gmail.com>
2018-11-22 21:52:18 -0800

    add notes

--- client/src/components/Hero/Search/Search.js
+++ client/src/components/Hero/Search/Search.js
@@ -1,5 +1,13 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
 import './Search.css';
+
+const testData = [
+  'Burger King', 
+  'McDonalds',
+  'Taco Bell'
+]
 
 class Search extends Component {
   state = {
@@ -15,29 +23,38 @@ class Search extends Component {
       query: this.search.value
     })
 
+    /**
+     * Every time this function fires, I need an algorithm to read the 
+     * current value in the input, and append elements from the 
+     * the data structure 'testData' above to the autocomplete 
+     * dropdown that match the value in input at that time
+     */
+
     if (this.search.value) { 
       document.getElementsByClassName('autocomplete')[0].style.visibility = 'visible'; 
     } else {
       document.getElementsByClassName('autocomplete')[0].style.visibility = 'hidden'; 
     }
-    /**
-     * Also, if input val = 0, do not show dropdown
-     */
   }
 
   render () {
     return (
       <form autoComplete="off">
-
-        <input ref={input => this.search = input} onChange={this.handleInputChange} type="search" name="search" className="search" placeholder="Try typing 'Burger King' or 'McDonald's'" />
+        <input 
+          // Refs provide a way to access DOM nodes (like <input />) 
+          // created in the render method.
+          ref={input => this.search = input} 
+          onChange={this.handleInputChange} 
+          type="search" 
+          name="search" 
+          placeholder="Try typing 'Burger King' or 'McDonald's'" />
         <input type="submit" value="Search" />
-        
+
         <div className="autocomplete">
           <ul>
             <li><a href="/">{this.state.query}</a></li>
           </ul>
         </div>
-
       </form>
     )
   }
