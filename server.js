572bb6a281c8f65d95690cd7a29e8e3deadd1ecd
Matthew Volk <volkmattj@gmail.com>
2019-02-03 11:39:03 -0800

    update comments

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 
 /* ----------------------- *\
-    Routes
+    API
 \* ----------------------- */
 app.use('/', require('./api'));
 
