c5c4799eaad468ce6dfab4235d503fe1cbf8e9c4
Matthew Volk <volkmattj@gmail.com>
2019-03-15 21:52:38 -0700

    add resource for /dropdown, add pseudocode

--- api/index.js
+++ api/index.js
@@ -30,18 +30,15 @@ router.post('/search', (req, res) => {
   pgConnection.connect()
    .then(() => {
 
-       // Create and Sanitize SQL Query
        const searchQuery = "SELECT * FROM restaurant_menu_items WHERE restaurant_name=$1 AND calories_kcal!='0' AND menu_item_category='Entrée' ORDER BY calories_kcal ASC, protein_grams DESC;"
        const searchQueryParams = [restaurantQuery];
 
-       // Execute SQL Query
        return pgConnection.query(searchQuery, searchQueryParams);
    })
    .then((results) => {
        // DEBUG:
        // console.log(results.rows);
 
-       // If Promise contains rows that have data, send those to the front end
        if (results.rows.length > 0) {
            res.send(results.rows);
        } else {
@@ -58,7 +55,26 @@ router.post('/search', (req, res) => {
 router.get('/search', (req, res) => {
   let restaurantQuery = req.query.q;
 
+  // connect to pg
+  // STRICT levanshtein fuzzystrmatch
+  // store in JSON object to be sent to front end
+
+  // connect to pg
+  // query DB for filterable terms (calories, protein, fat, carbs)
+  // store in same JSON object as results and send to front end
+
   res.json({ "Hello": restaurantQuery });
+});
+
+// TODO: Create GET for React search bar component dropdown suggestion list
+router.get('/dropdown/list' /* change to /dropdown-list? */, (req, res) => {
+  let restaurantQuerySoFar = req.data; // Get the data from the AJAX request
+
+  // connect to pg
+  // LOOSE levanshtein fuzzystrmatch 
+  // store results in JSON object and send to front end
+
+  res.json({/* if search bar has "taco": Taco Bell, Del Taco, Jack in the Box (J in the B would be tricky) */ restaurantQuerySoFar});
 });
 
 module.exports = router
