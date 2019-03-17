3786950acafc8dee8d2a3d7246f261e24ea66453
Matthew Volk <volkmattj@gmail.com>
2019-03-16 22:02:02 -0700

    add todo

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        {/* TODO: add <Results /> component to list search results, and a 
+        wrapper component to wrap <Search /> and <Results /> */}
         <Search />
       </div>
     );
